c067c49a27ae5ee1cfb0e1cdca14e7b7f5f2991b
fer <fer@fer-laptop.(none)>
2013-09-13 13:57:08 +0200

    No permite hacer búsquedas exactas de algo de la lista de underage

--- torrents/blueprints/files.py
+++ torrents/blueprints/files.py
@@ -557,7 +557,7 @@ def process_search_results(s=None, query=None, category=None, not_category=None,
     if canonical_query:
         prepared_phrase = blacklists.prepare_phrase(canonical_query.replace("_"," "))
         #si la query exacta está en underage no se muestra nada
-        if query in blacklists["underage"].texts:
+        if blacklists["underage"].exact(query):
             g.blacklisted_content = "Search"
         else:
             #Si solo la incluye ya tiene que completar con misconduct

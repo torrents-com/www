7029de7e3368a53b528f51a377e5a64aadb5ce21
leo <leo@mp2p.net>
2013-11-07 13:06:50 +0100

    Categories configuration changes.

--- torrents/__init__.py
+++ torrents/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 from collections import namedtuple
-Category = namedtuple('Category', ['cat_id','url','title','tag','content','content_main','show_in_home','subcats'])
+Category = namedtuple('Category', ['cat_id','url','title','tag','content','content_main','show_in_home'])

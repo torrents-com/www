895c847a8afce98716449b3f00fc852754b1441a
leo <leo@mp2p.net>
2013-11-13 18:00:48 +0100

    Home design changes.

--- torrents/services/categories.py
+++ torrents/services/categories.py
@@ -7,5 +7,5 @@ class CategoriesCache:
 
     def update_subcategories(self, subcategories):
         for category in self.categories:
-            category.subcategories[:] = [subcat for subcat, info in subcategories[category.tag].iteritems() if info.get("c",0)>0]
+            category.subcategories[:] = [subcat for subcat, info in sorted(subcategories[category.tag].iteritems(), key=lambda x:-x[1].get("c",0)) if info.get("c",0)>0]
             category.dynamic_tags[:] = [subcat.replace(" ","") for subcat in category.subcategories]

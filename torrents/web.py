2643161bfdafdb0d10372c2c639f9e3151a81061
leo <leo@mp2p.net>
2014-02-03 10:57:52 +0100

    Includes code to error pages to allow ban these on varnish.

--- torrents/web.py
+++ torrents/web.py
@@ -286,7 +286,7 @@ def create_app(config=None, debug=False):
 
     @allerrors(app, 400, 401, 403, 404, 405, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 500, 501, 502, 503)
     def all_errors(e):
-        g.cache_code = ""
+        g.cache_code = "E"
         g.last_modified = None
 
         error = e.code if hasattr(e,"code") else 500
